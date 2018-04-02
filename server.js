a110f0a7456e0b6438aeab9d7a5445836fd5546f
tintedsky <hongbonew@gmail.com>
2018-04-02 17:01:26 -0400

    express middleware for static webpage

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 var app = express();
 
+//express.static() take absolute path to the folder you want to server up.
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
   res.send({
     name: 'Moose',
     likes: [
@@ -22,4 +24,7 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Unable to handle request.'
   });
 });
-app.listen(3000);   //3000 is the port number in local machine.
+
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000.')
+});   //3000 is the port number in local machine.
