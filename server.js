38888b45a4c228fddbe7842bb93f037078e80f0e
tintedsky <hongbonew@gmail.com>
2018-04-01 22:00:17 -0400

    1. send back JSON data; 2. send back About html data; 3. send back Error data;

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express!</h1>');
+  //res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Moose',
+    likes: [
+      'Biking',
+      'Cities'
+    ]
+  });
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request.'
+  });
+});
 app.listen(3000);   //3000 is the port number in local machine.
