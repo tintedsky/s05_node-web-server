32791ca46bb104ed9da1f4cd4128cffbeb956104
tintedsky <hongbonew@gmail.com>
2018-04-03 22:28:48 -0400

    1. using middleware to control the execution flow. 2. Using fs to append content to a file 3. for appendFile(), the third argument is an arrow function 4. Using app.use() middleware to show maintenance page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -7,12 +8,32 @@ hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
 });
-hbs.registerHelper('screamIt', (text) =>{
+hbs.registerHelper('screamIt', (text)=>{
   return text.toUpperCase();
 });
 
 
 app.set('view engine', 'hbs');
+
+//If we do something asynchronous, the middleware is not going to move on. Only when we call next, The application will continue to run.
+app.use((req, res, next) => {  // next() tell express when the middleware function is done.
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+/*Comment the following function call out when maintenance is done.*/
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//app.use() register how you register a middleware and it takes a function as an argument.
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
